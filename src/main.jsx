65831327136011711758bec014e5582ed235bff5
lalit-tambe <lalittambe963@gmail.com>
2025-02-24 00:47:27 +0530

    Added UI & APIs for first page - districts list

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import DistrictPage from "./pages/DistrictPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/district/:districtCode" element={<DistrictPage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
